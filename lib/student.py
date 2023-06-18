6dad775e494951addf89a4a3d0efd6d13c35822a
Mai <garcia.ton94@gmail.com>
2023-06-18 16:00:56 -0400

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,9 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    knowledge = []
+
+    def learn(self, string):
+        self.knowledge.append(string)
